4b4308c798002285df16a8adc2cdab41545d0e7f
Cristian Rojas <reycristiancy@gmail.com>
2025-02-13 09:25:00 -0500

    feat: :sparkles: add months state to select

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Button from "./components/Button"
 import { formatMoney } from "./helpers";
 function App() {
   const [qty, setQty] = useState(10000);
-
+  const [months, setMonths] = useState(6);
   const MIN = 0;
   const MAX = 20000;
   const STEP = 100;
@@ -55,7 +55,7 @@ function App() {
         </p>
         <h2 className="text-2xl font-extrabold text-gray-500 text-center">Elige un <span className="text-indigo-600">Plazo</span> a pagar</h2>
 
-        <select className="mt-5 w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500" name="" id="">
+        <select className="mt-5 w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500" value={months} name="" id="" onChange={e => setMonths(+e.target.value)}>
           <option value="6">6 meses</option>
           <option value="12">12 meses</option>
           <option value="24">24 meses</option>
