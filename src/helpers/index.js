8bd9824ae1c613937462521a8898526555bba40d
Cristian Rojas <reycristiancy@gmail.com>
2025-02-13 09:39:50 -0500

    feat: :sparkles: add calculateTotalPay function

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -6,6 +6,32 @@ const formatMoney = (value) => {
     return formatter.format(value);
 }
 
+const calculateTotalPay=(qty, time) =>{
+    let total;
+
+    // the higher the quantity, the lower the interest rate
+    if(qty < 5000){
+        total = qty * 1.5;
+    }else if(qty >= 5000 && qty < 10000){
+        total = qty * 1.4;
+    }else if(qty >= 10000 && qty < 15000){
+        total = qty * 1.3;
+    }else{
+        total = qty * 1.2;
+    }
+    //time - more time, more interest rate
+    if(time === 6){
+        total*= 1.1;
+    }else if(time === 12){
+        total*= 1.2;
+    }else{
+        total*= 1.3;
+    }
+
+    return total;
+}
+
 export {
-     formatMoney
+     formatMoney,
+     calculateTotalPay
 }
